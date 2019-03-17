fcbb69f9ff9e84f95449691164a13ff4dcc5cd61
techfreks <48522802+techfreks@users.noreply.github.com>
2019-03-17 14:24:39 -0700

    Create TransactionRepository.java

---
+++ src/main/java/com/ismart/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.ismart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ismart.model.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
