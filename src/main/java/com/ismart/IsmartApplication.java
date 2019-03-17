f3f3254a91a1b60d1327914c3f072fe09797a93f
techfreks <48522802+techfreks@users.noreply.github.com>
2019-03-17 12:15:24 -0700

    Project Structure

---
+++ src/main/java/com/ismart/IsmartApplication.java
@@ -0,0 +1,13 @@
+package com.ismart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismart/IsmartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ismart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsmartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
