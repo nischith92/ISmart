020e20dbb43c6bda3896abaa2996e381200f28e6
techfreks <48522802+techfreks@users.noreply.github.com>
2019-03-17 19:46:48 -0700

    Final Commit

--- src/main/java/com/ismart/controller/TransactionController.java
+++ src/main/java/com/ismart/controller/TransactionController.java
@@ -1,16 +1,30 @@
 package com.ismart.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.jboss.logging.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ismart.model.Transaction;
@@ -26,6 +40,9 @@ public class TransactionController {
 	@Autowired
 	TransactionService transactionService;
 
+	@Autowired
+	PagingAndSortingRepository<Transaction, Long> repository;
+
 	@PostMapping("/makeTransaction")
 	public ResponseEntity<Transaction> saveTransaction(@Valid @RequestBody Transaction transaction){
 		logger.info("saveTransaction");
@@ -36,5 +53,21 @@ public class TransactionController {
 		return new ResponseEntity<Transaction>(response, HttpStatus.OK);
 	}
 
-
+	@GetMapping("/listTransaction")
+	public ResponseEntity<List<Transaction>> findTransactions(@RequestParam String period ,@RequestParam String spendCategory) {
+		
+		List<Transaction> data = new ArrayList<>();
+		
+		if(!spendCategory.equals("All")) {
+			data = transactionService.findBySpendCategory(spendCategory);
+			
+		} else if (period.equals("Current")) {
+		
+			Pageable pageable = PageRequest.of(0, 10,Sort.by("date").descending());
+			Page<Transaction> page = transactionService.listRecentTransaction(pageable);
+			data = page.getContent();
+		}
+		return new ResponseEntity<List<Transaction>>(data, HttpStatus.OK);
+		
+	}
 }
--- src/main/java/com/ismart/service/TransactionService.java
+++ src/main/java/com/ismart/service/TransactionService.java
@@ -1,9 +1,18 @@
 package com.ismart.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ismart.model.Transaction;
 
 public interface TransactionService {
 
 	Transaction saveTransaction(Transaction transaction);
+	
+	List<Transaction> findBySpendCategory(String spendCategory);
+	
+	Page<Transaction> listRecentTransaction(Pageable pageable);
 	
 }
